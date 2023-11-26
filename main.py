db5006faf04c7d06b43ed1e598e577941ddd6baf
377Harun <harun-bakirci@hotmail.com>
2023-11-26 22:33:14 +0300

    added which ip sended request

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 
-from fastapi import FastAPI , requests
+from fastapi import FastAPI , Request
 import joblib
 from data_models import Iris
 from data_models import Advertising
@@ -10,6 +10,8 @@ encoder_iris_loaded = joblib.load("models/iris_encoder.pkl")
 estimator_advertising_loaded = joblib.load("models/randomforest.pkl")
 
 #dir()
+
+
 
 
 app = FastAPI()
@@ -29,7 +31,7 @@ def make_iris_prediction(model ,encoder ,  request):
     return prediction_real[0]
 
 
-@app.post("/prediction/iris")  
+@app.post("/prediction/iris", tags=["İris Tahmin Blogu"])  
 async def predict_iris(request: Iris):
     prediction = make_iris_prediction(estimator_iris_loaded , encoder_iris_loaded , request.dict()) 
     return prediction
@@ -49,13 +51,29 @@ def make_advertising_prediction(model  ,  request):
     return prediction[0] 
  
 
-@app.post("/prediction/advertising")     
-async def predict_advertising(request: Advertising): 
-    prediction = make_advertising_prediction(estimator_advertising_loaded  , request.dict())
-    #burasi yalnizca deger dondurur index 0 almana gerek yok zaten make_advertising_prediction fonksiyonunda
-    # 0 indexi donduk
-    return {"sonuc": round(prediction,2) ,"yazar":"harun bakirci"} 
+@app.post("/prediction/advertising", tags=["advertising Tahmin blogu"])     
+async def predict_advertising(request: Advertising , fast_api_ip : Request): 
+    ip_adresi = fast_api_ip.client.host
+    if ip_adresi == "127.0.0.1" :
+     prediction = make_advertising_prediction(estimator_advertising_loaded  , request.dict())
+        #burasi yalnizca deger dondurur index 0 almana gerek yok zaten make_advertising_prediction fonksiyonunda
+        # 0 indexi donduk
+     return {"sonuc": round(prediction,2) ,"yazar":"harun bakirci" , "ip_adresi" : "dogru ip adresi"} 
+    elif ip_adresi != "127.0.0.1":
+     return "Yanlis İp Adresi"
+
+
+
+import time
+from typing import Dict
+
+import jwt
 
 
 
 
+@app.post("/prediction/advertising/host")
+async def predict_advertising_host(fast_api_req : Request):
+    return ("gelen ip adresi : "  + str(fast_api_req.client.host))
+
+ 
